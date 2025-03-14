28932152c496c1a3b0b6514efd34399560b9774b
Andres <andresgonzalez305423@correo.itm.edu.co>
2025-03-14 07:49:01 -0500

    inicializacion del proyecto

---
+++ modelo/producto.php
@@ -0,0 +1,11 @@
+<?php
+    class Producto{
+        string $id;
+        string $nombre;
+        int $stock;
+        float $valor_unitario;
+
+        public function _construct($id,$nombre,$stock,$valor_unitario)
+    }
+
+?>
